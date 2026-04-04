ad974cebac4ae4c305d672da1c1e81cbd23e7c5e
medhat <eng.medhathamada@gmail.com>
2026-04-04 23:54:46 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.dummyjson.com",
-        port: "",
+// module.exports = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "cdn.dummyjson.com",
+//         port: "",
        
-      },
-    ],
-  },
-};
+//       },
+//     ],
+//   },
+// };
+
+
+
+
+const nextConfig = {
+  output: 'standalone',
+}
+
+module.exports = nextConfig
